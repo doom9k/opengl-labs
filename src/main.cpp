3822cf4732187f59e9eafe9ad896fa711841451c
doom9k <tungentium@gmail.com>
2023-09-29 19:36:59 +0300

    lab1 init

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,95 @@
-#include <glad/glad.h>
+#include <glad/glad.h> /* Должна подключаться перед GLFW, т.к пероопределяет типы и функции */
 #include <GLFW/glfw3.h>
+
 #include <iostream>
+
+const int SCREEN_WIDTH  = 800;
+const int SCREEN_HEIGHT = 600;
+const char *WINDOW_NAME = "LAB #1";
 
 int main(void)
 {
-    GLFWwindow* window;
+	/************************
+		Инициализация GLFW 
+	*************************/
+	glfwInit();
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	/* Задается минимальная требуемая версия OpenGL (3.3) */
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); 
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+	/* Установка профайла для которого создается контекст 
+		Используяется CORE профиль. */
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+	/* Выключение возможности изменения размера окна */
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 
-    if (!gladLoadGL())
-    {
-        std::cerr << "Can't start GLAD" << std::endl;
-        return -1;
-    }
+	/*****************************************************
+		Создание и установка контекста для Объекта ОКНА
+	 *****************************************************/
 
-    std::cout << "OpenGL Version: " << GLVersion.major << "." << GLVersion.minor << std::endl;
+	/* Создание объекта окна, с параметрами, 
+	  (высота, ширина, название)*/
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_NAME, nullptr, nullptr);
+	if (window == nullptr) // Проверка, создалось ли окно
+	{
+		std::cerr << "Failed to created Window!" << std::endl;
+		glfwTerminate(); // Выключение GLFW
+		return -1;
+	}
+	/* Установка контекста GLFW, для ранее созданого окна */
+	glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClearColor(0.25f, 0.33f, 0.4f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+	/*********************************
+		Проверка инициализации GLAD
+	 *********************************/
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cerr << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	/* Вывод доп. информации о OpenGL в консоль */
+	std::cout << "Render: " << glGetString(GL_RENDERER) << std::endl;
+	std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+	/************************
+		Настройка VIEWPORT
+	 ************************/
 
-    glfwTerminate();
-    return 0;
+	/* Сообщаем OpenGL - размер отрисовываемого окна */
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+	
+
+
+	/***************************
+		Основной Цикл Рендера
+	 ***************************/
+
+
+	/* glfwWindowShouldClose() :: проверяет в начале каждой
+	  итерации цикла, получил ли GLFW сигнал, к закрытия 
+	  
+	   glfwPollEvents() :: Проверяет были ли вызваны какие либо события
+     (клавиатура или мышь, например) и вызывает установленные функции
+     (указатели на callback функции)
+
+       glfwSwapBuffers() :: Заменяет цветовой буфер 
+     (большой буффер, содержащий значения цвета дял каждого пикселя в окне),
+     показывает результат на экране.
+	  
+	 */
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	
+	/* Выходим из функции, очищаем выделенные нам ресурсы. */
+	glfwTerminate();
+	return 0;
 }
